e63a54bacec3a25d7149fae2b0733bb0982cb66b
Valentin Alberto <valalberto0214@gmail.com>
2024-11-02 15:39:00 +0800

    -configure datasource -add componentscan properties -add logging configuration

--- ems-api/src/main/java/com/via/ems/ems_api/EmsApiApplication.java
+++ ems-api/src/main/java/com/via/ems/ems_api/EmsApiApplication.java
@@ -2,8 +2,10 @@ package com.via.ems.ems_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.via.ems")
 public class EmsApiApplication {
 
 	public static void main(String[] args) {
